76c586af4d24e20a23f9f35ffc6fe4367e9b58be
Ashail33 <34643237+Ashail33@users.noreply.github.com>
2024-04-05 00:09:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -71,6 +71,10 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 
     # What does your project relate to?
