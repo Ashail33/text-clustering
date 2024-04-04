14f4c04edadca9e86821d6b92ca41b225ca32f99
Ashail33 <34643237+Ashail33@users.noreply.github.com>
2024-04-05 00:05:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='text clustering',
+    name='textclustering',
     version='1.0.0',
 
     description='',
